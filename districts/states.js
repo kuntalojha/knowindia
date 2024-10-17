81fb8054af6bdcc7fa03f8013d43ec2f92762494
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-10-17 23:48:07 +0530

    link states data

--- districts/states.js
+++ districts/states.js
@@ -1,10 +1,61 @@
+import andhraPradesh from './states/Andhra_Pradesh.js';
+import arunachalPradesh from './states/Arunachal_Pradesh.js';
+import assam from './states/Assam.js';
+import bihar from './states/Bihar.js';
+import chhattisgarh from './states/Chhattisgarh.js';
+import goa from './states/Goa.js';
+import gujarat from './states/Gujarat.js';
+import haryana from './states/Haryana.js';
+import himachalPradesh from './states/Himachal_Pradesh.js';
+import jharkhand from './states/Jharkhand.js';
+import karnataka from './states/Karnataka.js';
+import kerala from './states/Kerala.js';
+import madhyaPradesh from './states/Madhya_Pradesh.js';
 import maharashtra from './states/Maharashtra.js';
+import manipur from './states/Manipur.js';
+import meghalaya from './states/Meghalaya.js';
+import mizoram from './states/Mizoram.js';
+import nagaland from './states/Nagaland.js';
+import odisha from './states/Odisha.js';
+import punjab from './states/Punjab.js';
+import rajasthan from './states/Rajasthan.js';
+import sikkim from './states/Sikkim.js';
+import tamilNadu from './states/Tamil_Nadu.js';
+import telangana from './states/Telangana.js';
+import tripura from './states/Tripura.js';
+import uttarPradesh from './states/Uttar_Pradesh.js';
+import uttarakhand from './states/Uttarakhand.js';
 import westbengal from './states/West_Bengal.js';
 
 const states = {
-  WB: westbengal,
+  AP: andhraPradesh,
+  AR: arunachalPradesh,
+  AS: assam,
+  BR: bihar,
+  CG: chhattisgarh,
+  GA: goa,
+  GJ: gujarat,
+  HR: haryana,
+  HP: himachalPradesh,
+  JH: jharkhand,
+  KA: karnataka,
+  KL: kerala,
+  MP: madhyaPradesh,
   MH: maharashtra,
-  // Add more states as needed
+  MN: manipur,
+  ML: meghalaya,
+  MZ: mizoram,
+  NL: nagaland,
+  OD: odisha,
+  PB: punjab,
+  RJ: rajasthan,
+  SK: sikkim,
+  TN: tamilNadu,
+  TG: telangana,
+  TR: tripura,
+  UP: uttarPradesh,
+  UK: uttarakhand,
+  WB: westbengal,
 };
 
 export default states;
